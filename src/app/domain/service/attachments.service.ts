d9ee6a6dbea84bbeea170e809b3e6c31262b33ea
Dobrovolschi, Sergiu <sdobrovo@revenue.ie>
2017-11-23 17:52:17 +0000

    CHANGE: Display message when error occurs in services

--- src/app/domain/service/attachments.service.ts
+++ src/app/domain/service/attachments.service.ts
@@ -3,6 +3,8 @@ import {Note} from '../model/note';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpErrorResponse} from '@angular/common/http';
+import {MessageService} from 'primeng/components/common/messageservice';
 
 
 @Injectable()
@@ -10,8 +12,9 @@ export class AttachmentsService {
 
   private _notesBaseUrl = '/api/notes';
   private _attachments: Observable<Attachment[]>;
+  private _errorCount = 0;
 
-  constructor(private http: HttpClient) {}
+  constructor(private _http: HttpClient, private _msgService: MessageService) {}
 
   getNoteAttachments(note: Note): Observable<Attachment[]> {
 
@@ -23,9 +26,23 @@ export class AttachmentsService {
       const attachments: Attachment[] = [];
 
       attachmentIds.forEach((id: string) => {
-        this.http.get<Attachment>(this._notesBaseUrl + '/' + note.id + '/attachments/' + id + '/details').subscribe(
-          (result: Attachment) => {
-            attachments.push(result);
+        this._http.get<Attachment>(this._notesBaseUrl + '/' + note.id + '/attachments/' + id + '/details').subscribe(
+          (attachment: Attachment) => {
+            attachments.push(attachment);
+          },
+          (err: HttpErrorResponse) => {
+            this._msgService.add({
+              severity: 'warn', summary: 'Unable to load attachments',
+              detail: 'Error occurred during the load of attachments'
+            });
+            if (err.error instanceof Error) {
+              // A client-side or network error occurred. Handle it accordingly.
+              console.log('An error occurred:', err.error.message);
+            } else {
+              // The backend returned an unsuccessful response code.
+              // The response body may contain clues as to what went wrong,
+              console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+            }
           }
         );
       });
